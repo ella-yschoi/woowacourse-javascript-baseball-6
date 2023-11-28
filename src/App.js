50ac3d6c9a298b00b40b3b52ae906333c7e81027
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-28 16:09:37 +0900

    feat: 게임 시작 시 비즈니스 로직이 실행되도록 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import BaseballGame from './BaseballGame';
+import BaseballGame from './BaseballGame.js';
+import { LOG } from './common/constants.js';
+import { printMessage } from './common/utils.js';
 
 class App {
   constructor() {
     this.game = new BaseballGame();
   }
-  async play() {}
+  async play() {
+    printMessage(LOG.start_game);
+    await this.game.startGame();
+  }
 }
 
 export default App;
