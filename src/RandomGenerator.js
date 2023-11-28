3bcdf6aa84f808e2cafe1279496e48cba019fa79
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-28 16:09:10 +0900

    feat: 랜덤 숫자 생성기에 게임 재시작 로직 반영

--- src/RandomGenerator.js
+++ src/RandomGenerator.js
@@ -1,14 +1,23 @@
 import { Random } from '@woowacourse/mission-utils';
-import { GAME } from './common/constants';
+import { GAME } from './common/constants.js';
 
 class RandomGenerator {
+  constructor() {
+    this.generateComputerNumber = new this.generateNumber();
+  }
 
-  static generateComputerNumber() {
+  generateNewNumber() {
+    this.generateComputerNumber = new this.generateNumber();
+  }
+
+  generateNumber() {
     const numbers = new Set();
+
     while (numbers.size < GAME.size) {
       const number = Random.pickNumberInRange(GAME.min_count, GAME.max_count);
       numbers.add(number);
     }
+
     return Array.from(numbers).join('');
   }
 }
