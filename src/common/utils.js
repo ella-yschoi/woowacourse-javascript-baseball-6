3e69343d0a5eefb1599d33fcfd381c3a1f75a74c
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-28 14:04:58 +0900

    feat: 기능 구현에 사용될 유틸 함수들을 추가

---
+++ src/common/utils.js
@@ -0,0 +1,16 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { GAME, REGEX } from './constants.js';
+
+const printMessage = (message) => Console.print(message);
+
+const generateNumberInRange = (min, max) => Random.pickNumberInRange(min, max);
+
+const throwError = (message) => {
+throw new Error(message);
+};
+
+const isNumeric = (input) => REGEX.positive_integer.test(input);
+
+const isUniqueDigits = (input) => new Set(input.toString().split('')).size === GAME.size;
+
+export { printMessage, generateNumberInRange, throwError, isNumeric, isUniqueDigits };
